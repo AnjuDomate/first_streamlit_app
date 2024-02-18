d20259f81a16a2d7bb2110a08855c706f261a2c8
AnjuDomate <160400980+AnjuDomate@users.noreply.github.com>
2024-02-18 18:50:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breatfast Menu')
-streamlit.text(🥣'Omega3 & blueberry oatmeal')
-streamlit.text( 🥗'kale, spinch & Rocket Smoothie')
-streamlit.text(🐔'Hard Boiled Free Range Egg')
-streamlit.text(🥑🍞'Avocado and Bread')
+streamlit.text('🥣 Omega3 & blueberry oatmeal')
+streamlit.text('🥗 kale, spinch & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado and Bread')
 
 
