179e9cebf86dd956e6971a6c2c83397c062cb055
AnjuDomate <160400980+AnjuDomate@users.noreply.github.com>
2024-02-18 18:44:11 +0530

    Update streamlit_app.py
    added header and title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breatfast Menu')
 streamlit.title('Omega3 & blueberry oatmeal')
-streamlit.title('Omega3 & blueberry oatmeal')
-streamlit.title('Omega3 & blueberry oatmeal')
+streamlit.title('kale, spinch & Rocket Smoothie')
+streamlit.title('Hard Boiled Free Range Egg')
 
